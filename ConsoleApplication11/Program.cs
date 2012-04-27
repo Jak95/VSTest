97b6aea20774e4b3c1d39a71196350f47ebe7d90
Eugeny_Shcherbinin <jak95@mail.ru>
2012-04-27 16:31:28 +0400

    123

--- ConsoleApplication11/Program.cs
+++ ConsoleApplication11/Program.cs
@@ -7,6 +7,11 @@ namespace ConsoleApplication11
 {
   class Program
   {
+
+    /// <summary>
+    /// Main
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
     }
